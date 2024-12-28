6298aea6f045651a5f87837356c7142a882f259f
Manuel Mangalathu <manuelmangalathu@gmail.com>
2024-12-28 18:51:57 +0100

    modify user data model

--- src/main/java/com/lordstdpa/data/user/UserDataModel.java
+++ src/main/java/com/lordstdpa/data/user/UserDataModel.java
@@ -1,8 +1,14 @@
 package com.lordstdpa.data.user;
 
+import com.lordstdpa.domain.user.UserRole;
+
 public class UserDataModel {
     String userId;
     String firstName;
     String lastName;
     String userRole;
+
+    public String getRole() {
+        return userRole;
+    }
 }
