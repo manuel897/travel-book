4208acb48a38a5ee81452ab4fb1597184cc1577d
Manuel Mangalathu <manuelmangalathu@gmail.com>
2024-12-29 20:00:55 +0100

    add spring dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.example"
@@ -14,6 +16,11 @@ dependencies {
     testImplementation("org.mockito:mockito-core:5.14.2")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // spring
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
