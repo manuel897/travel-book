6b046fceb6ea06080a63eceeb618ace402483a0c
Manuel Mangalathu <manuelmangalathu@gmail.com>
2025-01-27 23:52:59 +0100

    remove unused imports

--- src/main/java/com/example/RestServiceApplication.java
+++ src/main/java/com/example/RestServiceApplication.java
@@ -1,26 +1,19 @@
 package com.example;
 
-import com.example.config.CustomConfig;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.transaction.TransactionManager;
 
 import javax.sql.DataSource;
-import java.io.FileInputStream;
-import java.util.Properties;
 
 @SpringBootApplication
 public class RestServiceApplication {
