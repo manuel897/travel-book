ba1cd25961493d87d1db02ec794f34aaabb4e87c
Manuel Mangalathu <manuelmangalathu@gmail.com>
2025-02-02 14:30:55 +0100

    add logging in user repo

--- src/main/java/com/example/data/user/UserRepositoryImpl.java
+++ src/main/java/com/example/data/user/UserRepositoryImpl.java
@@ -2,12 +2,16 @@
 package com.example.data.user;
 
 import com.example.domain.user.UserRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 @Component
 public class UserRepositoryImpl implements UserRepository {
+    private static final Logger LOGGER = LogManager.getLogger();
+
     private final UserDataSource userDataSource;
 
     UserRepositoryImpl(UserDataSource userDataSource) {
@@ -16,11 +20,15 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public Optional<UserDataModel> findByUsername(String username) {
+        LOGGER.trace("Find user with username `{}`", username);
+
         return userDataSource.findByUsername(username);
     }
 
     @Override
     public void createUser(UserDataModel user) {
+        LOGGER.info("Creating user with username `{}`", user.username);
+
         userDataSource.save(user);
     }
 }
